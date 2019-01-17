34c380017ced5da9b509456dd1fa26345b0c9720
Tomas Hudec <tomas.hudec@kiwi.com>
2019-01-17 10:36:31 +0100

    Add checks for duration input fields

--- phoenix/slackbot/views.py
+++ phoenix/slackbot/views.py
@@ -759,6 +759,17 @@ class DialogSubmissionHandler():
                 'name': 'started_at',
                 'error': 'Invalid format.',
             })
+        if started_at > resolved_at:
+            self.errors += [
+                {
+                    'name': 'started_at',
+                    'error': "Outage can't be resolved before it started. Please fix the Start and Resolve times.",
+                },
+                {
+                    'name': 'resolved_at',
+                    'error': "Outage can't be resolved before it started. Please fix the Start and Resolve times.",
+                },
+            ]
         if self.errors:
             return
 
