8045683965358771ad5c5ab08a27176863328dd5
Tomas Hudec <tomas.hudec@kiwi.com>
2019-06-25 13:29:49 +0200

    remove coala

--- local_testing.sh
+++ local_testing.sh
@@ -3,6 +3,5 @@
 docker_compose_config="-f docker-compose.yml -f docker-compose-tests.yml"
 
 docker-compose $docker_compose_config up -d
-docker-compose $docker_compose_config exec app pylint phoenix
 docker-compose $docker_compose_config exec app pytest
 docker-compose $docker_compose_config down
